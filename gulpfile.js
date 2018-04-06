45bf2cf6d5393a0ab885fed3f93a60d6c9f87659
Damanage <derongar@gmail.com>
2018-04-06 14:57:26 +0300

    gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,38 @@
-var gulp = require('gulp');
-gulp.task('bigjoe', () => {
-    console.log('task is loading')
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    imageMin    = require('gulp-imagemin'),
+    pngq        = require('imagemin-pngquant');
+
+gulp.task('sass', () => {
+    return  gulp.src('app/sass/main.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('browser-sync', () => {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
+});
+
+gulp.task('img', ()=>{
+    gulp.src('app/img/*')
+    .pipe(imageMin({
+        interlaced: true,
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngq()]
+    }))
+    .pipe(gulp.dest('dist/img/'))
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], () => {
+    gulp.watch('app/sass/main.sass', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/*.js', browserSync.reload);
 })
